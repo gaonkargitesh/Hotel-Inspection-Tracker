cbb900711f4de4ddd4e1ea83e1a3d48f2b667002
Gitesh Gaonkar <giteshgaonkar3@gmail.com>
2018-12-09 01:32:47 +0530

    Login Screen , Signup Screen and Launcher screens are designed and added.....

--- app/src/main/java/com/example/rahul/hit/launcher/view/LauncherActivity.java
+++ app/src/main/java/com/example/rahul/hit/launcher/view/LauncherActivity.java
@@ -1,10 +1,15 @@
 package com.example.rahul.hit.launcher.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.rahul.hit.BaseActivity;
 import com.example.rahul.hit.R;
+import com.example.rahul.hit.login.view.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LauncherActivity extends BaseActivity {
 
@@ -12,12 +17,22 @@ public class LauncherActivity extends BaseActivity {
     public static boolean isLogin;
     public static boolean value;
 
+    /*Timer timer;
 
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-
+        /*timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(LauncherActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },800);*/
     }
 
 
--- app/src/main/java/com/example/rahul/hit/login/view/LoginActivity.java
+++ app/src/main/java/com/example/rahul/hit/login/view/LoginActivity.java
@@ -1,22 +1,33 @@
 package com.example.rahul.hit.login.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.rahul.hit.R;
+import com.example.rahul.hit.signup.view.SignUpActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView signupTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        signupTextView=findViewById(R.id.loginPage_SignUp_TextView);
+        signupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 
 }
